11c3ed579cc31f06c6071399b62c06fc62233fbc
Ameen Al Mahozi <ameenalmahozi@gmail.com>
2022-04-08 17:38:24 +0300

    create a component for vertical nav

--- resources/js/Components/VerticalNavItem.vue
+++ resources/js/Components/VerticalNavItem.vue
@@ -3,7 +3,8 @@
 </script>
 
 <template>
-	<div class="px-6 py-6 text-white text-xl">
-		<slot></slot>
+	<div class="px-6 py-6 text-white text-2xl flex items-center">
+		<slot name="icon"></slot>
+		<slot name="text"></slot>
 	</div>
 </template>
