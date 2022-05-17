8cabbeab625ec2563e6e784b742bd975d68f9860
Ameen Al Mahozi <ameenalmahozi@gmail.com>
2022-05-17 20:09:01 +0300

    add page for tasks list

---
+++ resources/js/Pages/Tasks/TaskList.vue
@@ -0,0 +1,14 @@
+<script setup>
+	import AppLayout from '@/Layouts/AppLayout.vue';
+
+	defineProps({
+    team: Object,
+});
+
+</script>
+
+<template>
+	<AppLayout title="Team Tasks">
+		<h1>{{ team.name }} Tasks List</h1>
+	</AppLayout>
+</template>
