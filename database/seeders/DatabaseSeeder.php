bdb68d443da999e9b7d22359e873a1d3d2e9da17
Ameen Al Mahozi <ameenalmahozi@gmail.com>
2023-02-18 19:31:48 +0300

    improve seeder to create users when refreshing database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Team;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +17,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $user = User::create([
+            'name' => 'Ameen Al Mahozi',
+            'email' => 'ameenalmahozi@gmail.com',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+        ]);
+
+        $team = Team::Create([
+            'name' => 'Ameen Team',
+            'user_id' => $user->id,
+            'personal_team' => true,
+        ]);
+
+        User::factory(10)->create();
     }
 }
