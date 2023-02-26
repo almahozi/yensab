80175e742f3e341a6af78a268bbffebd65b6cbb1
Ameen Al Mahozi <ameenalmahozi@gmail.com>
2023-02-26 20:17:29 +0300

    rename file to match routes and controller actions

--- resources/js/Pages/Tasks/Index.vue
+++ resources/js/Pages/Tasks/Index.vue
@@ -5,19 +5,17 @@ import { Link } from '@inertiajs/inertia-vue3';
 import DataTable from 'primevue/datatable';
 import Column from 'primevue/column';
 import Card from 'primevue/card';
-import Button from 'primevue/button';
 
 
 const props = defineProps({
 	team: Object,
 	tasks: Object
 });
-console.log(props.team);
 </script>
 
 <template>
 	<AppLayout title="Team Tasks">
-		<div class="flex justify-center pt-12">
+		<div class="flex justify-center">
 			<div class="flex flex-col w-4/5 justify-center">
 				<div>
 					<JetButton><Link :href="route('tasks.create', team)">NEW TASK</Link></JetButton>
@@ -27,10 +25,17 @@ console.log(props.team);
 					<Card>
 						<template #content>
 							<DataTable :value="tasks">
-								<Column field="title" header="Title"></Column>
+								<Column field="title" header="Title">
+									<template #body="slotProps">
+										<Link :href="route('tasks.show', [props.team, slotProps.data.id])">{{ slotProps.data.title }}</Link>
+									</template>
+								</Column>
 								<Column field="assignee.name" header="Assignee"></Column>
-								<Column field="status" header="Status"><Badge></Badge></Column>
+								<Column field="status" header="Status"></Column>
 								<Column field="due_date" header="Due Date"></Column>
+								<template #empty>
+									No records found.
+								</template>
 							</DataTable>
 						</template>
 					</Card>
