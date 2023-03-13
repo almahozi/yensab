2eb4b76084e5ba873475c3bcd27fe47059133587
Ameen Al Mahozi <ameenalmahozi@gmail.com>
2023-03-13 22:08:18 +0300

    add test for reassigning tasks in updates

--- tests/Feature/CreateUpdateTest.php
+++ tests/Feature/CreateUpdateTest.php
@@ -38,4 +38,26 @@ class CreateUpdateTest extends TestCase
         $this->assertEquals('This is a message', $task->fresh()->updates()->first()->message);
 
     }
+
+    public function test_tasks_can_be_reassigned_in_updates()
+    {
+        $owner = User::factory()->hasOwnedTeams(1)->create();
+        $this->actingAs($owner);
+        $team = $owner->ownedTeams()->first();
+        $user = User::factory()->create();
+        $team->users()->attach($user);
+        $task = Task::factory()->recycle($owner)
+            ->recycle($team)
+            ->create();
+
+        $this->assertEquals($owner->id, $task->fresh()->assignee->id);
+
+        $response = $this->post(route('updates.store', $task), [
+            'message'   => 'This is a message',
+            'assignee'  => $user->id
+        ]);
+
+        $this->assertCount(1, $task->fresh()->updates);
+        $this->assertEquals($user->id, $task->fresh()->assignee->id);
+    }
 }
