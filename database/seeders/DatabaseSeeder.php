526e4b0eb5e8b50b2ff7ec62b66d1f3ae001887c
Ameen Al Mahozi <ameenalmahozi@gmail.com>
2023-05-02 21:04:34 +0300

    Make seed data generic.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,15 +18,15 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $user = User::create([
-            'name' => 'Ameen Al Mahozi',
-            'email' => 'ameenalmahozi@gmail.com',
+            'name' => 'User',
+            'email' => 'user@example.com',
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
         ]);
 
         $team = Team::Create([
-            'name' => 'Ameen Team',
+            'name' => 'Personal Team',
             'user_id' => $user->id,
             'personal_team' => true,
         ]);
